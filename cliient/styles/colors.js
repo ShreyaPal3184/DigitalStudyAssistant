3bd0f00687bc1603614dfd51d9b48743d997a1df
Prithvi Singh Chauhan <prithvi.ch.79@gmail.com>
2024-12-09 17:01:16 +0530

    resolved sub modules

---
+++ cliient/styles/colors.js
@@ -0,0 +1,9 @@
+export default {
+    primary: '#4caf50', // Change as needed
+    secondary: '#2196f3',
+    background: '#f5f5f5',
+    text: '#333333',
+    buttonText: '#ffffff',
+    inputBackground: '#ffffff',
+  };
+  
